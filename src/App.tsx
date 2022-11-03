fb89ba6ceb6127bb4879fc411ce1cfa13c244653
Antoine Fricker <antoine@marmelab.com>
2022-11-03 02:54:31 +0100

    App title context

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import { Editor } from "./editor/Editor";
 function App() {
   return (
     <div className="app">
-      <header className="app-header">header</header>
+      <header className="app-header">
+        <h1>SVG Filters editor</h1>
+      </header>
       <main className="app-container">
         <Editor />
       </main>
